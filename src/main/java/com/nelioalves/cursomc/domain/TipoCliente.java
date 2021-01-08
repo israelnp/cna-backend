3b7ac2790fa95ca5f3ff46dda324fb0b8f12a63f
Israel Pereira <israelpereira1984@gmail.com>
2021-01-08 18:35:14 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/nelioalves/cursomc/domain/TipoCliente.java
@@ -0,0 +1,39 @@
+package com.nelioalves.cursomc.domain;
+
+public enum TipoCliente {
+
+    PESSOAFISICA(1, "Pessoa Física"),
+    PESSOAJURICA(2, "Pessoa Jurídica");
+
+
+    private Integer cod;
+    private String descricao;
+
+    TipoCliente(Integer cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public Integer getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static TipoCliente toEnum(Integer cod) {
+
+        if (cod == null) {
+            return null;
+        }
+
+        for (TipoCliente x : TipoCliente.values()) {
+            if (cod.equals(x.getCod())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Id inválido: " + cod);
+    }
+}
