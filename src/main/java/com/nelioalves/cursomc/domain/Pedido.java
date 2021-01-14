d258b53dd820c14e0e3eaf74fe2218b15385b8d1
israelnp <israelpereira1984@gmail.com>
2021-01-14 16:59:49 -0300

    Subtotal e total do Pedido

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -30,7 +30,6 @@ public class Pedido  implements Serializable {
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
 
-    @JsonIgnore
     @OneToMany(mappedBy = "id.pedido")
     private Set<ItemPedido> itens = new HashSet<>();
 
@@ -93,6 +92,13 @@ public class Pedido  implements Serializable {
         this.itens = itens;
     }
 
+    public double getValorTotal() {
+        double soma = 0.0;
+        for (ItemPedido ip : itens) {
+            soma = soma + ip.getSubTotal();
+        }
+        return soma;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
